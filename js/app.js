15a682dd105e645ee2c41a5b8dab1e987934740d
German Fica <germafica@gmail.com>
2019-02-21 13:14:08 -0300

    List Rendering

--- js/app.js
+++ js/app.js
@@ -6,5 +6,17 @@ var app = new Vue({
     description: "image",
     link: "https://germanfica.xyz/",
     inventory: 100,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 });
